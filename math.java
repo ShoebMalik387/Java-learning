285e936f52d4487ee98ec11a3ab67d50ef49d38c
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-18 01:30:17 +0530

    Add files via upload

---
+++ math.java
@@ -0,0 +1,23 @@
+public class math {
+   
+    public static void main (String[] args) {
+
+        int a,b,c,d,e;
+        a = 10;
+        b = 2;
+        c = 5;
+        a = a+b;
+        b = a-b;
+        c = a*b;
+        d = a%b;
+        e = a/c;
+
+        System.out.println ("a+b:"+a);
+        System.out.println ("a-b:"+c);
+        System.out.println ("a*b:"+b);
+        System.out.println ("a/b:"+d);
+
+        System.out.println ("Mathematical Opreation are Performed");
+
+    }
+}
