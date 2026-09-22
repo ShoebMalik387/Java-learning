9cf2d401f4670092e4a210d7ee0d3d9f7fb4d2df
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-23 00:36:29 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,12 @@
+class Factorial {
+
+ public static void main(String[] args) {
+
+        long fact = 1;
+        for (int i = 1; i<=5; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial is:"+ fact);
+ }
+    
+}
