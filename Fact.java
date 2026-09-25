1cdf6a8e93b30449ac98a0888c7a42011b9be453
Shoeb Akhtar <malikshoeb924@gmail.com>
2026-09-26 02:06:43 +0530

    Add files via upload

---
+++ Fact.java
@@ -0,0 +1,16 @@
+ class Fact {
+
+    public static void main (String [] args) {
+
+        int  a = 1, b = 7, Fact = 1;
+
+        while (a <= b) {
+            
+            Fact *= a;
+            a++;
+    }
+
+    System.out.println("Factorial is:"+Fact);
+  }
+    
+}
